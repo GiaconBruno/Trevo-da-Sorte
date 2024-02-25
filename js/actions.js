17c029d921dbabc52b09bcecd37fcbc44ef6fd68
Bruno <56009821+GiaconBruno@users.noreply.github.com>
2024-02-25 05:41:30 -0300

    v2

---
+++ js/actions.js
@@ -0,0 +1,123 @@
+let game = [];
+let rounds = [];
+let luck = [];
+
+const validate = () => {
+  start.classList.remove('border-danger');
+  end.classList.remove('border-danger');
+  qtdNum.classList.remove('border-danger');
+  return ((parseInt(end.value) + 1) - parseInt(start.value)) >= parseInt(qtdNum.value);
+}
+
+const Gerar = () => {
+  const valid = validate();
+  if (!valid) {
+    start.classList.add('border-danger');
+    end.classList.add('border-danger');
+    qtdNum.classList.add('border-danger');
+    return
+  };
+  result.innerHTML = '';
+  prepare.classList.remove('d-none');
+  load.classList.remove('d-none');
+  gerar.setAttribute('disabled', true);
+
+  setTimeout(() => {
+    mount();
+    plus();
+    show();
+    load.classList.add('d-none');
+  }, 10);
+  gerar.removeAttribute('disabled');
+}
+
+const mount = () => {
+  const nStart = parseInt(start.value)
+  const nEnd = parseInt(end.value)
+  const nNum = parseInt(qtdNum.value)
+  const nRound = parseInt(qtdRound.value)
+  game = [];
+  rounds = [];
+  // Gera os numeros automaticos
+  while (game.length < nRound) {
+    while (rounds.length < nNum) {
+      const num = parseInt(Math.random() * ((nEnd + 1) - nStart) + nStart);
+      const find = rounds.find(n => num == n);
+      if (find == undefined) rounds.push(num);
+    }
+    rounds.sort((a, b) => a - b);
+    game.push(rounds);
+    rounds = [];
+  }
+}
+
+const plus = () => {
+  luck = [];
+  const group = game.flat()
+  const plus = [];
+  group.map((num, x) => {
+    if (x == 0) plus.push({
+      num,
+      count: 1
+    })
+    else {
+      const pos = plus.findIndex(lNum => lNum.num == num);
+      if (pos != -1) plus[pos].count++;
+      else plus.push({
+        num,
+        count: 1
+      })
+    }
+  })
+
+  plus.sort((a, b) => {
+    if (a.count < b.count) return 1
+    if (a.count > b.count) return -1
+    return 0
+  })
+
+  luck = plus.map(p => p.num).slice(0, parseInt(qtdNum.value))
+}
+
+const show = () => {
+  result.innerHTML = '';
+  const cG = Math.max(String(game.length).length, 2)
+  const cN = Math.max(String(end.value.length).length, 2)
+  const icon = '<i class="bi small bi-lightning-charge-fill w-auto px-0"></i>';
+  luck.sort((a, b) => a - b)
+
+  result.innerHTML =
+    `<div id="visibleLuck" class="row mx-0 my-3 align-items-center justify-content-center ${(!ckLuck.checked)?'d-none':''}}">` +
+    `${(luck.includes(luck[0]+luck[1]))?icon:''}<div class="col-auto d-flex px-0">${better(luck.map(l => `<p class="fw-bold">${String(l).padStart(cN,0)}</p>`),'|')}</div></div>`;
+  result.innerHTML += better(game.map((r, x) =>
+    `<div class="row mx-0 align-items-center justify-content-center">` +
+    `<div class="col-4 px-0 fw-bold">Rodada ${String(x+1).padStart(cG,0)}:${(r.includes(r[0]+r[1]))?icon:''}</div>` +
+    `<div class="col-auto d-flex px-0">${better(r.map(n => `<p ${luck.includes(n)?'class="plus"':''}>${String(n).padStart(cN,0)}</p>`),' ')}</div></div>`), '')
+}
+
+const Clear = () => {
+  start.classList.remove('border-danger');
+  end.classList.remove('border-danger');
+  qtdNum.classList.remove('border-danger');
+
+  start.removeAttribute('disabled');
+  end.removeAttribute('disabled');
+  qtdNum.removeAttribute('disabled');
+  qtdRound.removeAttribute('disabled');
+  gerar.removeAttribute('disabled');
+
+  prepare.classList.add('d-none');
+  result.innerHTML = '';
+}
+
+const showLuck = () => {
+  if (!document.querySelector('#visibleLuck')) return;
+  const pLuck = Array.from(document.querySelectorAll('.plus'));
+  gerar.focus();
+  if (ckLuck.checked)[visibleLuck.classList.remove('d-none'), pLuck.map(p => p.classList.remove('clear'))];
+  else [visibleLuck.classList.add('d-none'), pLuck.map(p => p.classList.add('clear'))];
+}
+
+const better = (payload, simble = '') => {
+  return String(payload).replace(/,/g, simble)
+}
--- js/main.js
+++ js/main.js
@@ -1,116 +1,121 @@
-
 function Gerar() {
-    //variaveis
-    var round = document.getElementById("round").value;
-    var num = document.getElementById("num").value;
-    var ent = document.getElementById("de").value;
-    var out = document.getElementById("ate").value;
-    var res = document.getElementById("res");
-    var dup = 0; roda = 0; t = 0;
-    res.innerHTML = "";
-    var dados = []; more = [];
+  //variaveis
+  // var round = document.getElementById("round").value;
+  // var num = document.getElementById("num").value;
+  // var ent = document.getElementById("de").value;
+  // var out = document.getElementById("ate").value;
+  // var res = document.getElementById("res");
+  var dup = 0;
+  roda = 0;
+  t = 0;
+  res.innerHTML = "";
+  var dados = [];
+  more = [];
 
-    // Gera varias rres.innerHTML=dados;odadas
-    for (y = 0; y < round; y++) {
-        // Gera os numeros automaticos
-        while (roda < num) {
-            var n = parseInt(Math.random() * 100);
-            // Verifica se o numero aleatorio esta entre o min e max
-            if ((n >= ent) && (n <= out)) {
-                // Confere se o numero ja foi sorteado
-                for (c = 0; c < roda; c++) {
-                    if (n == dados[c]) {
-                        dup++;
-                    }
-                }
-                // Salva se o numero for unico
-                if (dup == 0) {
-                    if (n < 10) {
-                        n = "0" + n;
-                    }
-                    dados[roda] = n;
-                    roda++;
-                    more[t] = n;
-                    t++;
-                }
-                dup = 0;
-            }
+  // Gera varias rodadas
+  for (y = 0; y < round; y++) {
+    // Gera os numeros automaticos
+    while (roda < num) {
+      var n = Math.random() * (max - min) + min;
+      // Verifica se o numero aleatorio esta entre o min e max
+      if ((n >= ent) && (n <= out)) {
+        // Confere se o numero ja foi sorteado
+        for (c = 0; c < roda; c++) {
+          if (n == dados[c]) {
+            dup++;
+          }
         }
-        roda = 0;
-        // Ordenas os numeros e exibe
-        res.innerHTML = res.innerHTML + "<b>Rodada " + (y + 1) + ": ";
-        res.innerHTML = res.innerHTML + dados.sort() + "<br>";
-        res.innerHTML = res.innerHTML.replace(/,/g, " ");
+        // Salva se o numero for unico
+        if (dup == 0) {
+          if (n < 10) {
+            n = "0" + n;
+          }
+          dados[roda] = n;
+          roda++;
+          more[t] = n;
+          t++;
+        }
+        dup = 0;
+      }
     }
-
-    //Gera os mais sorteados
-    m = 1; s = 0;
-    mord = []; mcont = []; mfull = [];
-    //if (luck==1){
-
-    //Procura repeditos e conta
-    for (i = ent; i <= out; i++) {
-        for (c = 0; c < (round * num); c++) {
-            if ((more[c].valueOf()) == i) {
-                m++;
-                mord[c] = m;
-            }
-        }
-        m = 0;
-    }
-    //Guarda a posicao dos mais sorteados
-    for (b = round; b >= 0; b--) {
-        for (z = 0; z <= (round * num); z++) {
-            if (b == mord[z]) {
-                mcont[s] = z;
-                s++;
-            }
-        }
-    }
-    //Recebe valor dos mais sorteados
-    a = 0; mf = 0;
-    for (y = 0; y < num;) {
-        for (h = 0; h <= y; h++) {
-            if (mfull[h] == more[mcont[a]]) {
-                mf++;
-            }
-        }
-        if (mf == 0) {
-            mfull[y] = (more[mcont[a]]);
-            y++;
-        }
-        a++;
-        mf = 0;
-    }
-
+    roda = 0;
     // Ordenas os numeros e exibe
-    mfull.sort();
-    rod.innerHTML = "<br><b>Mais sorteados: <br>" + mfull;
-    rod.innerHTML = rod.innerHTML.replace(/,/g, " ");
-    More();
-    //}
+    res.innerHTML = res.innerHTML + "<b>Rodada " + (y + 1) + ": ";
+    res.innerHTML = res.innerHTML + dados.sort() + "<br>";
+    res.innerHTML = res.innerHTML.replace(/,/g, " ");
+  }
+
+  //Gera os mais sorteados
+  m = 1;
+  s = 0;
+  mord = [];
+  mcont = [];
+  mfull = [];
+  //if (luck==1){
+
+  //Procura repeditos e conta
+  for (i = ent; i <= out; i++) {
+    for (c = 0; c < (round * num); c++) {
+      if ((more[c].valueOf()) == i) {
+        m++;
+        mord[c] = m;
+      }
+    }
+    m = 0;
+  }
+  //Guarda a posicao dos mais sorteados
+  for (b = round; b >= 0; b--) {
+    for (z = 0; z <= (round * num); z++) {
+      if (b == mord[z]) {
+        mcont[s] = z;
+        s++;
+      }
+    }
+  }
+  //Recebe valor dos mais sorteados
+  a = 0;
+  mf = 0;
+  for (y = 0; y < num;) {
+    for (h = 0; h <= y; h++) {
+      if (mfull[h] == more[mcont[a]]) {
+        mf++;
+      }
+    }
+    if (mf == 0) {
+      mfull[y] = (more[mcont[a]]);
+      y++;
+    }
+    a++;
+    mf = 0;
+  }
+
+  // Ordenas os numeros e exibe
+  mfull.sort();
+  rod.innerHTML = "<br><b>Mais sorteados: <br>" + mfull;
+  rod.innerHTML = rod.innerHTML.replace(/,/g, " ");
+  More();
+  //}
 }
 
 function More() {
-    var luck = document.getElementById("luck").checked;
-    var color = document.getElementById("res");
-    var num = document.getElementById("num").value;
-    var rod = document.getElementById("rod").innerHTML;
-    var c = "";
+  var luck = document.getElementById("luck").checked;
+  var color = document.getElementById("res");
+  var num = document.getElementById("num").value;
+  var rod = document.getElementById("rod").innerHTML;
+  var c = "";
 
-    if (luck == 1) {
-        for (i = 27; i < (num * 3) + 27; i = (i + 3)) {
-            c = rod.substring(i, i + 2);
-            // console.log(" I: " + i + " C: " + c );
-            var re = new RegExp(c, 'g');
-            color.innerHTML = color.innerHTML.replace(re, "<ii style=background-color:lightgreen;>" + c + "</ii> ");
-            document.getElementById("rod").style.display = 'block';
-        }
-    } else {
-        document.getElementById("rod").style.display = 'none';
-        color.innerHTML = color.innerHTML.replace(/<ii style="background-color:lightgreen;">/g, "");
-        color.innerHTML = color.innerHTML.replace(/<\ii>/g, "");
-        color.innerHTML = color.innerHTML.replace(/  /g, " ");
+  if (luck == 1) {
+    for (i = 27; i < (num * 3) + 27; i = (i + 3)) {
+      c = rod.substring(i, i + 2);
+      // console.log(" I: " + i + " C: " + c );
+      var re = new RegExp(c, 'g');
+      color.innerHTML = color.innerHTML.replace(re, "<ii style=background-color:lightgreen;>" + c + "</ii> ");
+      document.getElementById("rod").style.display = 'block';
     }
-
+  } else {
+    document.getElementById("rod").style.display = 'none';
+    color.innerHTML = color.innerHTML.replace(/<ii style="background-color:lightgreen;">/g, "");
+    color.innerHTML = color.innerHTML.replace(/<\ii>/g, "");
+    color.innerHTML = color.innerHTML.replace(/  /g, " ");
+  }
 }
